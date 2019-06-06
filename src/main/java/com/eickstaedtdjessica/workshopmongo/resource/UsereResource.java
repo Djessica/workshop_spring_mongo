cd8f6dca98843bc6b56089d5c692a64961f7c663
Djéssica <djessica.eickstaedt@compasso.com.br>
2019-06-06 17:00:56 -0300

    Padrão DTO para retornar usuários

--- src/main/java/com/eickstaedtdjessica/workshopmongo/resource/UsereResource.java
+++ src/main/java/com/eickstaedtdjessica/workshopmongo/resource/UsereResource.java
@@ -1,6 +1,8 @@
 package com.eickstaedtdjessica.workshopmongo.resource;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eickstaedtdjessica.workshopmongo.domain.User;
+import com.eickstaedtdjessica.workshopmongo.dto.UserDTO;
 import com.eickstaedtdjessica.workshopmongo.services.UserService;
 
 //informa que essa classe é um recurso rest
@@ -23,12 +26,12 @@ public class UsereResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	//responseentity retorna resposta http
-	public ResponseEntity<List<User>> getAll(){
+	public ResponseEntity<List<UserDTO>> getAll(){
 		
 		List<User> lista = service.findAll();
-		
+		List<UserDTO> listDto = lista.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		//ok instancia o responseentity com o código de resposta http
 		//o corpo da resposta vai trazer a lista
-		return ResponseEntity.ok().body(lista);
+		return ResponseEntity.ok().body(listDto);
 	}
 }
