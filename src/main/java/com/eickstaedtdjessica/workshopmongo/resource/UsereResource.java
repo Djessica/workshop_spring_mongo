e91a4df0e958e78884e23a4b0c36f99951b4cc5d
Djéssica <djessica.eickstaedt@compasso.com.br>
2019-06-10 16:50:29 -0300

    Atualização de usuário com PUT

--- src/main/java/com/eickstaedtdjessica/workshopmongo/resource/UsereResource.java
+++ src/main/java/com/eickstaedtdjessica/workshopmongo/resource/UsereResource.java
@@ -69,5 +69,15 @@ public class UsereResource {
 
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+
+		return ResponseEntity.noContent().build();
+	}
 
 }
