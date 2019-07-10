08c73342f8d5c804de91bc00c9e76015fbf6b7b2
Djessica <djessica.eickstaedt@compasso.com.br>
2019-07-10 17:53:52 -0300

    consulta com varios criterios

--- src/main/java/com/eickstaedtdjessica/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/eickstaedtdjessica/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.eickstaedtdjessica.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -19,10 +20,18 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 
-	//o método tem esse nome porque ele usa critérios prontos do mongodb, nesse caso ele vai usar o
-	//findBy do mongo
-	//nome do que eu quero pesquisar
-	//containing do mongo
-	//ignoreCase do mongo
+	/*o método tem esse nome porque ele usa critérios prontos do mongodb, nesse caso ele vai usar o
+	findBy do mongo
+	nome do que eu quero pesquisar
+	containing do mongo
+	ignoreCase do mongo*/
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	/* Buscar posts contendo um dado string em qualquer lugar 
+	 * (no título, corpo ou comentários) e em um dado intervalo de datas*/
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2}},"
+			+ "  { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, "
+						+ "{  'body': { $regex: ?0, $options: 'i' }},"
+						+ "{  'comments.text': { $regex: ?0, $options: 'i' } } ]  } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/eickstaedtdjessica/workshopmongo/resource/PostResource.java
+++ src/main/java/com/eickstaedtdjessica/workshopmongo/resource/PostResource.java
@@ -1,27 +1,19 @@
 package com.eickstaedtdjessica.workshopmongo.resource;
 
-import java.net.URI;
-import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eickstaedtdjessica.workshopmongo.domain.Post;
-import com.eickstaedtdjessica.workshopmongo.domain.User;
-import com.eickstaedtdjessica.workshopmongo.dto.UserDTO;
 import com.eickstaedtdjessica.workshopmongo.resource.utils.URL;
 import com.eickstaedtdjessica.workshopmongo.services.PostService;
-import com.eickstaedtdjessica.workshopmongo.services.UserService;
 
 //informa que essa classe é um recurso rest
 //requestmapping informa o endpoint
@@ -55,6 +47,23 @@ public class PostResource {
 		text = URL.urlDecode(text);
 		
 		List<Post> lista = service.findByTitle(text);
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+		text = URL.urlDecode(text);
+		
+		//o new Date(0L) cria uma data default do java
+		Date min = URL.convertDate(minDate, new Date(0L));
+		// new date gera uma data com o instante atual do sistema
+		Date mx = URL.convertDate(maxDate, new Date());
+		
+		List<Post> lista = service.fullSearch(text, min, mx);
 		return ResponseEntity.ok().body(lista);
 	}
 
